931bce8c887f9d757c734ab806133cdbf8535257
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-05-11 11:50:23 +0530

    Few more demos

--- 0002SpringSecurity4CustomLoginFormAnnotation/src/main/java/com/sbk/customform/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.sbk.customform.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
